8a21b0b6f2b3aede3688bbea4e05fff9f31641dd
jaxcoder / <codenamejason@outlook.com>
2023-02-07 13:38:47 -0500

    WIP - feat: adding basic search feature (#7)
    * feat: adding basic search feature
    
    * update front end form for search
    
    * example artist image display
    
    * add flex and some ui updates
    
    * add missing file for vercel build

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -29,7 +29,7 @@ import externalContracts from "./contracts/external_contracts";
 // contracts
 import deployedContracts from "./contracts/hardhat_contracts.json";
 import { getRPCPollTime, Transactor, Web3ModalSetup } from "./helpers";
-import { Home, ExampleUI, Hints, Subgraph } from "./views";
+import { Home, ExampleUI, Hints, Subgraph, Jukebox } from "./views";
 import { useStaticJsonRPC, useGasPrice } from "./hooks";
 
 const { ethers } = require("ethers");
@@ -316,6 +316,9 @@ function App(props) {
         <Menu.Item key="/exampleui">
           <Link to="/exampleui">ExampleUI</Link>
         </Menu.Item>
+        <Menu.Item key="jukebox">
+          <Link to="/jukebox">Jukebox</Link>
+        </Menu.Item>
         <Menu.Item key="/mainnetdai">
           <Link to="/mainnetdai">Mainnet DAI</Link>
         </Menu.Item>
@@ -328,6 +331,9 @@ function App(props) {
         <Route exact path="/">
           {/* pass in any web3 props to this Home component. For example, yourLocalBalance */}
           <Home yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
+        </Route>
+        <Route exact path="/jukebox">
+          <Jukebox />
         </Route>
         <Route exact path="/debug">
           {/*
---
+++ packages/react-app/src/views/Jukebox.jsx
@@ -0,0 +1,145 @@
+import { Button } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Jukebox() {
+  const [results, setResults] = useState([]);
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  // const fetchTracks = (albumId, callback) => {
+  //   fetch({
+  //     url: "https://api.spotify.com/v1/albums/" + albumId,
+  //     success: function (response) {
+  //       callback(response);
+  //     },
+  //   });
+  // };
+
+  // const searchAlbums = query => {
+  //   fetch({
+  //     url: "https://api.spotify.com/v1/search",
+  //     data: {
+  //       q: query,
+  //       type: "album",
+  //     },
+  //     success: function (response) {
+  //       // todo: add error handling
+  //       console.log("response", response);
+  //       setResults(response);
+  //     },
+  //   });
+  // };
+
+  const searchArtists = async () => {
+    // todo: move to backend
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: searchKey,
+        type: "artist",
+      },
+    });
+
+    setArtists(data.artists.items);
+  };
+
+  const onQueryChange = e => {
+    console.log("e.target.value", e.target.value);
+    setSearchKey(e.target.value);
+  };
+
+  const submitSearch = e => {
+    e.preventDefault();
+    console.log("searching for", searchKey);
+    searchArtists(searchKey);
+  };
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find(elem => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const renderArtists = () => {
+    return artists.map(artist => {
+      console.log("artist", artist);
+      return (
+        <div
+          style={{ height: "200px", width: "200px", padding: "5px", display: "flex", flexGrow: "initial" }}
+          key={artist.id}
+        >
+          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+          {artist.name}
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div class="container">
+      {!token ? (
+        <a
+          style={{ marginTop: "20px" }}
+          href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <div>
+          <button style={{ marginTop: "20px", color: "black" }} onClick={logout}>
+            Logout
+          </button>
+          <h1>Search for an Artist</h1>
+          <p>
+            Type an artist name and click on "Search". Then, click on any album from the results to play 30 seconds of
+            its first track.
+          </p>
+          <div id="search-form">
+            <input
+              style={{ color: "black" }}
+              type="text"
+              onChange={e => {
+                onQueryChange(e);
+              }}
+              value={searchKey}
+              placeholder="Type an Artist Name"
+            />
+            <Button
+              onClick={e => {
+                submitSearch(e);
+              }}
+            >
+              Search
+            </Button>
+          </div>
+          <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", padding: "5px" }}>
+            {renderArtists()}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Jukebox;
