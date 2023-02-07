860b1686b437b3ab02e4c169c405e635ecfeda2c
Jaxcoder <codenamejason@outlook.com>
2023-02-07 13:50:03 -0500

    update sample env, add token contract update sol v

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -281,7 +281,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.4",
+        version: "0.8.17",
         settings: {
           optimizer: {
             enabled: true,
