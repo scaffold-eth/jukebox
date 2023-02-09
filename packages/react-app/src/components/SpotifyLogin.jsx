48651eff0940361869ef8b409ab24d0b42b80049
realcee <frasier@hotmail.fr>
2023-02-09 16:27:14 +0100

    Added login

---
+++ packages/react-app/src/components/SpotifyLogin.jsx
@@ -0,0 +1,13 @@
+function SpotifyLogin() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="btn-spotify" href="/auth/login">
+          Login with Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default SpotifyLogin;
