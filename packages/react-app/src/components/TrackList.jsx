1dc760cfd536c84414b56a063a9000ecb5c13f00
AntFrasier <frasier@hotmail.fr>
2023-02-09 19:21:44 +0100

    reset TrackList component

--- packages/react-app/src/components/TrackList.jsx
+++ packages/react-app/src/components/TrackList.jsx
@@ -1,72 +1,26 @@
-import { List, Progress, Button } from "antd";
-import SpotifyWebApi from "spotify-web-api-node";
-import { useEffect } from "react";
+import { List } from "antd";
+import TrackCard from "./TrackCard";
 
-const spotifyApi = new SpotifyWebApi({
-  client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-});
-
-function TrackCard({ track, token }) {
-  useEffect(() => {
-    if (!token) return;
-    spotifyApi.setAccessToken(token);
-    console.log(token);
-  }, [token]);
-
-  async function addToQueue(track) {
-    console.log("Added to queue :", track.uri);
-    try {
-      await spotifyApi.addToQueue([track.uri]);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async function play(track) {
-    console.log("play :", track.uri);
-    try {
-      await spotifyApi.play({ uris: [track.uri] });
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  function millisToMinutesAndSeconds(millis) {
-    const minutes = Math.floor(millis / 60000);
-    const seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  }
-  console.log("track", track);
-  const { id, artists, album, duration_ms: duration, name } = track;
+function TrackList({ tracks, token }) {
   return (
-    <List.Item
-      key={id}
-      extra={
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <img width={100} alt={name} src={album.images[0].url} />
-          <span>Duration: {millisToMinutesAndSeconds(duration)}</span>
-        </div>
-      }
-    >
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-        <div>
-          <span>Popularity:</span>
-          <Progress percent={track.popularity} />
-        </div>
-        <div>
-          <span>Track: {name}</span>
-        </div>
-        <div>
-          <span>Artist: {artists[0].name}</span>
-        </div>
-        <div>
-          <span>Album: {album.name}</span>
-        </div>
-        <Button onClick={() => addToQueue(track)}>Add to Queue</Button>
-        <Button onClick={() => play(track)}>Play</Button>
-      </div>
-    </List.Item>
+    <List
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+      }}
+      itemLayout="vertical"
+      size="large"
+      pagination={{
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 5,
+      }}
+      dataSource={tracks}
+      renderItem={item => <TrackCard track={item} token={token} />}
+    />
   );
 }
 
-export default TrackCard;
+export default TrackList;
